b62a46391c434f7a1a4921a5ed0e98e51a8b27c8
UfXpri <cfi2000@naver.com>
2022-07-07 06:40:48 +0900

    add click

--- web_server/app/main.py
+++ web_server/app/main.py
@@ -1,4 +1,6 @@
 
+import logging
+import pdb
 from flask import request
 from flask import Flask
 from flask import url_for
@@ -10,15 +12,15 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == 'POST':
-        print(request.form)
         username = request.form['username']
         return render_template('click.html', username = username)
     return render_template('main.html')
 
-@app.route("/click")
-def click(choice):
-    logging.info(f"user:{username} browser:{browser} hold_time:{hold_time}")
-    return True
+@app.route("/click", methods=["POST"])
+def click():
+    json_data = request.get_json()
+    logging.info(f"user:{json_data['username']} agent:{request.headers['User-Agent']} hold_time:{json_data['hold_time']}")
+    return "True"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8080)

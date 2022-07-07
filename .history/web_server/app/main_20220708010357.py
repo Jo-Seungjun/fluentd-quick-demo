17426f5afc6f3eddb905c02002c3ee520de5d81a
UfXpri <cfi2000@naver.com>
2022-07-08 03:28:44 +0900

    finally working

--- .history/web_server/app/main_20220708010357.py
+++ .history/web_server/app/main_20220708010357.py
@@ -1,27 +1,26 @@
 
+import pdb
 import logging
-from flask import Flask
 from flask import request
-from flask import Blueprint
+from flask import Flask
+from flask import url_for
+from flask import redirect
 from flask import render_template
 
-bp = Blueprint('main', __name__)
+app = Flask(__name__)
 
-@bp.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == 'POST':
         username = request.form['username']
         return render_template('click.html', username = username)
     return render_template('main.html')
 
-@bp.route("/click", methods=["POST"])
+@app.route("/click", methods=["POST"])
 def click():
     json_data = request.get_json()
     logging.info(f"user:{json_data['username']} agent:{request.headers['User-Agent']} hold_time:{json_data['hold_time']}")
     return "True"
 
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(bp)
-    return app
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=8080)
